c51da757e8e37699ad017b3d7f0497c37a71ba09
hdknr <gmail@hdknr.com>
2015-08-22 04:15:09 +0000

    Export User or Profile from Admin UI

---
+++ sample/profiles/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+import views
+
+urlpatterns = [
+    url(r'^export/profile', views.export_profile,
+        name="profiles_export_profile"),
+    url(r'^export/user', views.export_user,
+        name="profiles_export_user"),
+]
--- sample/profiles/views.py
+++ sample/profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
+import models
+from djuploader.csvutils import CsvResponse, CsvQuerySet
 
-# Create your views here.
+
+@staff_member_required
+def export_profile(request):
+    response = CsvResponse(filename="profile.csv")
+    models.Profile.csv.export(
+        response, relates=['user.username', 'user.email', ], encoding="cp932")
+    return response
+
+
+@staff_member_required
+def export_user(request):
+    response = CsvResponse(filename="user.csv")
+    CsvQuerySet(User).export(response, encoding="cp932")
+    return response
