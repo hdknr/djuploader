d47bbc679f9aa97d4e278be0e61ef09224894458
hdknr <gmail@hdknr.com>
2015-08-25 07:14:50 +0000

    Celery + RabbitMQ jobq

--- sample/app/apps.py
+++ sample/app/apps.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.apps import AppConfig as DjangoAppConfig
 from django.utils.translation import (
     ugettext_lazy as _,
 )
+from queue import CeleryLoader
 
 
 class AppConfig(DjangoAppConfig):
     name = 'app'
     verbose_name = _("Application")
+
+    def ready(self):
+        self.celery = CeleryLoader.create()
