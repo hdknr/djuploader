b22e7d2300b4fb14e287826e3e3a6334556762b6
hdknr <gmail@hdknr.com>
2015-10-22 22:21:10 +0000

    Simplify export() with header_row() and date_row()

--- lib/djuploader/queryset.py
+++ lib/djuploader/queryset.py
@@ -13,46 +13,64 @@ class UploadQuerySet(models.QuerySet):
     def get_field(self, model, name):
         return model._meta.get_field_by_name(name)
 
-    def export(self,
-               stream, format="csv", header=True, excludes=[], relates=[],
-               **kwargs):
+    def get_names(self, excludes=[]):
+        def _cache():
+            self._names = dict(
+                (field.name, field)
+                for field in self.model._meta.fields
+                if field.name not in excludes
+            )
+        return getattr(self, '_names', _cache())
 
-        if format == 'csv':
-            writer = csvutils.CsvWriter(stream, **kwargs)
-        else:
-            writer = xlsxutils.XlsxWriter(stream, **kwargs)
+    def get_related_model_fields(self, relates=[]):
+        def _cache():
+            self._related_model_fields = {}
+            for m, f in [r.split('.') for r in relates]:
+                self._related_model_fields[m] = \
+                    self._related_model_fields.get(m, []) + [f]
+        return getattr(self, '_related_model_fields', _cache())
 
-        names = dict(
-            (field.name, field)
-            for field in self.model._meta.fields
-            if field.name not in excludes
-        )
+    def header_row(self, excludes=[], relates=[]):
+        def _cache():
+            names = self.get_names(excludes=excludes)
 
-        related_models = {}
-        for m, f in [r.split('.') for r in relates]:
-            related_models[m] = related_models.get(m, []) + [f]
+            self._header_row = [
+                field.verbose_name for name, field in names.items()]
 
-        if header:
-            cols = [field.verbose_name for name, field in names.items()]
-
-            for rfn, rcs in related_models.items():
-                cols += [
+            related_model_fields = self.get_related_model_fields(relates)
+            for rfn, rcs in related_model_fields.items():
+                self._header_row += [
                     u"{0}.{1}".format(
                         rfn, self.field_verbose_name(
                             names[rfn].related_model, i))
                     for i in rcs]
 
-            writer.writerow(cols)
+        return getattr(self, '_header_row', _cache())
 
-        for row in self.all():
-            cols = [
-                getattr(row, name, None) or ''
-                for name, field in names.items() if name not in excludes]
+    def data_row(self, instance):
+        cols = [
+            getattr(instance, name, None) or ''
+            for name, field in self.get_names().items()]
 
-            for rfn, rcs in related_models.items():
-                value = getattr(row, rfn)
-                cols += [value and getattr(value, i, None) or '' for i in rcs]
+        for rfn, rcs in self.get_related_model_fields().items():
+            value = getattr(instance, rfn)
+            cols += [value and getattr(value, i, None) or '' for i in rcs]
+        return cols
 
+    def export(self,
+               stream, format="csv", header=True, excludes=[], relates=[],
+               **kwargs):
+
+        writer = {
+            'csv': csvutils.CsvWriter,
+            'xlsx': xlsxutils.XlsxWriter, }[format](stream, **kwargs)
+
+        if header:
+            writer.writerow(
+                self.header_row(excludes=excludes, realtes=relates))
+
+        for instance in self.all():
+            cols = self.data_row(instance)
             writer.writerow(cols)
 
         writer.close()
